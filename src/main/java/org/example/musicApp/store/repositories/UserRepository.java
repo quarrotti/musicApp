d327a24eaaf6d32cbcd3474e158f0029d23cb30a
quarrotti <quarrotti@gmail.com>
2024-12-01 20:54:18 +0300

    1.12 (2)

--- src/main/java/org/example/musicApp/store/repositories/UserRepository.java
+++ src/main/java/org/example/musicApp/store/repositories/UserRepository.java
@@ -2,10 +2,16 @@ package org.example.musicApp.store.repositories;
 
 import org.example.musicApp.store.entities.aboutUser.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByEmail(String email);
+
     boolean existsByEmail(String email);
+    @Query(value = "SELECT * FROM app_user ORDER BY SIZE(u.tracks) DESC")
+    List<UserEntity> findTop10UsersByAudioCount();
+
 }
